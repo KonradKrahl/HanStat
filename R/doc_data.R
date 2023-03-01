48cecf8aa3934a05882c5c25d553734b45abfb88
KonradKrahl <38402603+KonradKrahl@users.noreply.github.com>
2023-03-01 16:14:52 +0100

    code new

--- R/doc_data.R
+++ R/doc_data.R
@@ -1,20 +1,12 @@
-#' Radomized data for testing models
-#'
+#' Randomized data for testing models
 #' Contains 5 Variables, one dependent, 4 independent. The fourth independent is correlated with the dependent
-#'
 #' @docType data
-#'
 #' @usage data(data)
-#'
 #' @format data.frame
-#'
 #' @keywords datasets
-#'
 #' @references K.T.Krahl (2023)
-#'
-#' @source \href{https://www.hanseatic-statistics.de/}
-#'
+#' @source https://www.hanseatic-statistics.de
 #' @examples
 #' data(data)
-#' \donttest{LinReg('dp',c('iv_1','iv_2','iv_3',iv_4),data, bootstrap = FALSE, Number_Bootstrapps=1000, outlier_controll = FALSE, plot=TRUE)}
+#' \donttest{LinReg('dv',c('iv_1','iv_2','iv_3'),data=data,BS=FALSE,NBS=1000,OC=FALSE,plot=TRUE)}
 "data"
